1762da0ed1d4c4b689666cab63b807c17a0ff8a2
puaykai <puaykai@gmail.com>
2013-07-06 00:19:10 +0800

    Second Try

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -1,4 +1,14 @@
 
+/**
+ * This class is my first attempt at using Git on Eclipse!
+ * 
+ * Helllo GitHub!
+ * */
 public class HelloGit {
+	
+	public static void main(String[] args){
+		
+		System.out.println("I am using GitHub!");
+	}
 
 }
